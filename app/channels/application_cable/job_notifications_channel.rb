5ee36952c13485a6b5642be7db94f77e4ee5e616
Ketan singh <ketan02am@gmail.com>
2025-06-03 12:17:41 +0000

    zetwik error fixed

--- app/channels/application_cable/job_notifications_channel.rb
+++ app/channels/application_cable/job_notifications_channel.rb
@@ -1,5 +1,7 @@
-class JobNotificationsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_for "job_notifications_user_#{current_user.id}"
+module ApplicationCable
+  class JobNotificationsChannel < Channel
+    def subscribed
+      stream_for "job_notifications_user_#{current_user.id}"
+    end
   end
 end
