97deaec8081e6935c866394790146f00c727e851
Ketan singh <ketan02am@gmail.com>
2025-06-03 11:35:11 +0000

    some change

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const babelLoader = environment.loaders.get('babel')
-const options = babelLoader.use[0].options
-
-options.plugins = [
-  ...(options.plugins || []),
-  '@babel/plugin-proposal-optional-chaining',
-  '@babel/plugin-proposal-nullish-coalescing-operator',
-  '@babel/plugin-proposal-logical-assignment-operators'
-]
-
 module.exports = environment
